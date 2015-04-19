33c528e5b38c9384a2fb4e369d076bf8aa70f831
Aaron Saunders <aaron@clearlyinnovative.com>
2015-04-18 20:45:59 -0400

    playing with buttons

--- movieScreen.js
+++ movieScreen.js
@@ -6,15 +6,24 @@ var {
 	PixelRatio,
 	ScrollView,
 	StyleSheet,
+	TouchableHighlight,
 	Text,
 	View,
 } = React;
 
 var MovieScreen = React.createClass({
+	cameraBtnPressed : function() {
+		alert("camera button pressed");
+	},
 	render: function() {
 		var value = JSON.stringify(this.props.movie,null,2);
 		return (
 			<ScrollView contentContainerStyle={styles.contentContainer}>
+				<TouchableHighlight style={styles.button}
+				    underlayColor='#99d9f4'
+				    onPress={this.cameraBtnPressed}>
+				  <Text style={styles.buttonText}>Open Camera</Text>
+				</TouchableHighlight>
 				<View style={styles.mainSection}>
 					<Text  style={styles.movieYear} numberOfLines={100}>
 					{value}
@@ -24,6 +33,7 @@ var MovieScreen = React.createClass({
 		);
 	},
 });
+
 
 var styles = StyleSheet.create({
 	contentContainer: {
@@ -35,5 +45,22 @@ var styles = StyleSheet.create({
 	movieYear: {
 		top: 5,
 	},
+	buttonText: {
+	  fontSize: 18,
+	  color: 'white',
+	  alignSelf: 'center'
+	},
+	button: {
+	  height: 36,
+	  flex: 1,
+	  flexDirection: 'row',
+	  backgroundColor: '#48BBEC',
+	  borderColor: '#48BBEC',
+	  borderWidth: 1,
+	  borderRadius: 8,
+	  marginBottom: 10,
+	  alignSelf: 'stretch',
+	  justifyContent: 'center'
+	}
 });
 module.exports = MovieScreen;
